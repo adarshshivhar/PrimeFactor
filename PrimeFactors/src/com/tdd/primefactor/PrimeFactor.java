233a693a67806a2100c18ca4cd66499d49d92f17
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-02 21:56:04 +0530

    for 9 we did some changes and now our code is complete

--- PrimeFactors/src/com/tdd/primefactor/PrimeFactor.java
+++ PrimeFactors/src/com/tdd/primefactor/PrimeFactor.java
@@ -8,18 +8,15 @@ public class PrimeFactor {
 	public List<Integer> factorOf(int n) {
 		List<Integer> factors = new ArrayList<>();
 		int remainder = n;
-		if(remainder>1) {
-			while(remainder%2==0) {
-				factors.add(2);
-				remainder = remainder/2;
+		int divisor = 2;
+		
+		while(remainder>1) {
+			
+			while(remainder%divisor==0) {
+				factors.add(divisor);
+				remainder = remainder/divisor;
 			}
-			while(remainder%3==0) {
-				factors.add(3);
-				remainder = remainder/3;
-			}
-			if(remainder>1) {
-				factors.add(remainder);
-			}	
+			divisor++;
 		}
 		return factors;
 	}
