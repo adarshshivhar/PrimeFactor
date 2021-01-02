64f48c19812b3f2cea138bd88ecc49be5af70dc9
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-02 21:49:42 +0530

    for 7 also test pass

--- PrimeFactors/test/com/tdd/primefactor/PrimeFactorTest.java
+++ PrimeFactors/test/com/tdd/primefactor/PrimeFactorTest.java
@@ -20,5 +20,6 @@ public class PrimeFactorTest {
 		assertEquals(new ArrayList<Integer>(Arrays.asList(2,2)), pf.factorOf(4));
 		assertEquals(new ArrayList<Integer>(Arrays.asList(5)), pf.factorOf(5));
 		assertEquals(new ArrayList<Integer>(Arrays.asList(2,3)), pf.factorOf(6));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(7)), pf.factorOf(7));
 	}
 }
